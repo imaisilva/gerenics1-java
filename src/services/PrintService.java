976b224d37c0a1751e6f7b58910bb1c5715e7851
Mayara Silva <alohasp@outlook.com>
2023-10-17 15:03:54 -0300

    Generic print service

--- src/services/PrintService.java
+++ src/services/PrintService.java
@@ -3,15 +3,15 @@ package services;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PrintService {
+public class PrintService <T> {
 	
-	private List<Integer> list = new ArrayList<>();
+	private List<T> list = new ArrayList<>();
 	
-	public void addValue(Integer value) {
+	public void addValue(T value) {
 		list.add(value);
 	}
 	
-	public Integer first() {
+	public T first() {
 		if (list.isEmpty()) {
 			throw new IllegalStateException("List is empty");
 		}
@@ -23,7 +23,7 @@ public class PrintService {
 		if (!list.isEmpty()) {
 			System.out.print(list.get(0));
 		}
-		for (int i=1; 1<list.size(); i++) {
+		for (int i=1; i<list.size(); i++) {
 			System.out.print(", " + list.get(i));
 		}
 		System.out.println("]");
